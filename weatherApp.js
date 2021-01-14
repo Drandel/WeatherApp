0d539e5b808ec805f11e3c838aa413ba9c7feae6
dee.randel <dee.randel@gmail.com>
2021-01-14 16:18:30 -0700

    Initial Commit
    Signed-off-by: dee.randel <dee.randel@gmail.com>

---
+++ weatherApp.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    var apiKey = 'bd560fcc516e28b7bcc4ebb782fb5b13';
+    var zipcode = '80920';
+    var url = 'api.openweathermap.org/data/2.5/weather?zip=' + zipcode + '&appid=' + apiKey + '';
+    console.log(url)
+
+    $.ajax({
+
+        
+        url: url,
+        type: 'GET',
+        data: {},
+        success:function(data) {
+            console.log(data);
+        },
+        error: function(errorThrown){
+            console.log(errorThrown);
+        }
+    });
+})
