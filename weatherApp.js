795089ac5c212fc5222185a44b4f43235f4dd9c9
dee.randel <dee.randel@gmail.com>
2021-01-14 18:27:31 -0700

    Signed-off-by: dee.randel <dee.randel@gmail.com>

--- weatherApp.js
+++ weatherApp.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
 
+    $('#getWeather').on('click', function(){
+        api()
+    })
+
+    
+})
+
+function api() {
     var apiKey = 'bd560fcc516e28b7bcc4ebb782fb5b13';
     var zipcode = '80920';
     var url = 'api.openweathermap.org/data/2.5/weather?zip=' + zipcode + '&appid=' + apiKey + '';
     console.log(url)
-
     $.ajax({
         url: url,
         type: 'GET',
@@ -17,4 +24,4 @@ $(document).ready(function(){
             console.log(errorThrown);
         }
     });
-})
+}
